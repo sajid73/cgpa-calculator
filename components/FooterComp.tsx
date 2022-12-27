3366d535d095fe2f34acd08f7498e1cb9cf85f0d
sajid73 <smsajid7300@gmail.com>
2022-12-27 15:06:01 +0600

    bug fixed

--- components/FooterComp.tsx
+++ components/FooterComp.tsx
@@ -7,8 +7,8 @@ const FooterComp = () => {
     return (
         <Footer className='footer'>
             <p id='connect'><span id='connectText'>connect with me</span></p>
-            <div style={{ margin: '5px 0' }}><a href="https://github.com/sajid73" target="_blank"><Image alt='github' style={{ marginBottom: '-4px', marginRight: '5px' }} src={github} width='20' height={'20'} /><span style={{ height: '25px' }}>sajid73</span></a></div>
-            <div><a href="https://www.linkedin.com/in/sajid73"><Image alt='linked' style={{ marginBottom: '-2px', marginRight: '5px' }} src={linkedin} width='15' height={'15'} /><span style={{ height: '25px' }}>sajid73</span></a></div>
+            <div style={{ margin: '5px 0' }}><a rel="noreferrer" href="https://github.com/sajid73" target="_blank"><Image alt='github' style={{ marginBottom: '-4px', marginRight: '5px' }} src={github} width='20' height={'20'} /><span style={{ height: '25px' }}>sajid73</span></a></div>
+            <div><a rel="noreferrer" href="https://www.linkedin.com/in/sajid73"><Image alt='linked' style={{ marginBottom: '-2px', marginRight: '5px' }} src={linkedin} width='15' height={'15'} /><span style={{ height: '25px' }}>sajid73</span></a></div>
         </Footer>
     );
 };
--- components/GpaShow.tsx
+++ components/GpaShow.tsx
@@ -7,9 +7,6 @@ const GpaShow = ({ pos, semesters }: any) => {
     useEffect(() => {
         setCg(gpa(semesters));
     }, [semesters])
-    useEffect(() => {
-        setCg(gpa(semesters));
-    }, [])
     return (
         <div style={{ textAlign: pos, color: 'cyan', margin: '1.5rem 5px' }}>
             {isNaN(cg) ? (<span>Add course</span>) : (<span>Your CGPA is <b>{cg.toPrecision(4)}</b></span>)}
